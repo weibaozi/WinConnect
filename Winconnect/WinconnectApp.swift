c2260c6c6f007d54e6361b3647273506828c726e
SeanLiang-gif <123599069+SeanLiang-gif@users.noreply.github.com>
2023-06-06 14:38:14 -0700

    Add files via upload

---
+++ Winconnect/WinconnectApp.swift
@@ -0,0 +1,18 @@
+//
+//  WinconnectApp.swift
+//  Winconnect
+//
+//  Created by Shawn Liang on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WinconnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+	
